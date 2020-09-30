bddf487d8bce07996a5385ec816262b0d8996697
John <jpending@gmail.com>
2020-09-30 14:04:24 -0400

    added more unnecessary styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Form, FormControl, InputGroup, Button} from 'react-bootstrap'
+import FadeIn from './FadeIn.js'
 class App extends React.Component {
   state={
     currVal: null,
     valArr: [],
+    useArr: [],
     setArr: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5,
     ],
     count: null,
-    bool: ''
+    bool: '',
+    whichArr: 'mine',
   }
 
   handleChangeVal(e) {
@@ -30,8 +33,13 @@ class App extends React.Component {
   treeAddVals=(func) => {
     this.state.valArr.forEach(val => {func(val)})
   }
+  handleSwitchArr=() => {
+    this.state.whichArr==='mine'? this.setState({useArr: this.state.setArr, whichArr: 'yours'}):this.setState({useArr: this.state.valArr, whichArr: 'mine'});
+  }
   renderSetArr=() => {
-    return this.state.setArr.map((val, i) => <h4 className="flex-1 font-bold text-xl px-2 m-2 w-1/12 bg-blue-500 rounded-full hover:bg-yellow-300 transition duration-100 ease-in-out transform hover:-translate-y-1 hover:scale-125" onClick={(e) => this.selectFromArr(e)} id={val} key={i+1}>{val}</h4>)
+    return this.state.useArr.map((val, i) =>
+      <FadeIn delay={100*i} transitionDuration={10*i} key={(i*100)+'c'}><h4 className="flex flex-1 font-bold text-xl text-center justify-center m-1 p-4 px-4 w-1/8 bg-blue-500 rounded-full hover:bg-yellow-300 transition duration-100 ease-in-out transform hover:-translate-y-1 hover:scale-125" onClick={(e) => this.selectFromArr(e)} id={val} key={i+1}>
+        {val}</h4></FadeIn>)
   }
   arrSort=(arr) => {
     let newArr=arr.sort((a, b) => {return a-b});
@@ -111,10 +119,13 @@ class App extends React.Component {
         </Form>{' '}
 
 
-        <div className="align-center px-40 mt-8">
-          <h2>Our Array</h2>
+        <div className="align-center px-20 m-8 rounded-full border-transparent border-dotted border-8 bg-gradient-to-b from-transparent via-teal-200 to-transparent">
+          <h2>Current Array</h2>
+          <Button onClick={this.handleSwitchArr} className="m-2 px-2 border rounded shadow">Switch Array</Button>
           <div className="flex flex-row flex-wrap max-w-md ">
+
             {this.renderSetArr()}
+
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 
-  <App />
+  <App key={89998329} />
   ,
   document.getElementById('root')
 );
